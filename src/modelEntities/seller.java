2bc644f196231953240f4e778f4aa083426299a0
Luisa Larrieu <larrieuestudos@gmail.com>
2024-10-18 14:43:16 -0300

    Classe-Entidade Seller

---
+++ src/modelEntities/seller.java
@@ -0,0 +1,90 @@
+package modelEntities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class seller implements Serializable{
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+    private department department;
+
+    public seller(){
+    }
+
+    public seller(Integer id, String name, String email, Date birthDate, Double baseSalary, modelEntities.department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Date getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+    public modelEntities.department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(modelEntities.department department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        seller seller = (seller) o;
+        return id.equals(seller.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", baseSalary=" + baseSalary +
+                ", department=" + department +
+                '}';
+    }
+}
--- src/program.java
+++ src/program.java
@@ -1,7 +1,14 @@
 import modelEntities.department;
+import modelEntities.seller;
+
+import java.util.Date;
+
 public class program {
     public static void main(String[] args){
         department obj = new department(1, "Books");
         System.out.println(obj);
+        seller seller = new seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0,obj);
+
+        System.out.println(seller);
     }
 }
